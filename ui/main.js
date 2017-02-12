d7c51ef8ac5389ffee1386b65d328579704fce4b
Aditya Tibrewal <adityatibrewal11@gmail.com>
2017-02-13 00:56:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 //Move the image
-var img = document.getElementById('img');
+var img = document.getElementById('mudi');
 img.onclick = funcrion () {
     img.style.marginLeft = '100px'; 
 }
