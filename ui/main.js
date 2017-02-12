9cca25ffd2818f52e430157acba017c204fd057b
Aditya Tibrewal <adityatibrewal11@gmail.com>
2017-02-13 00:55:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//Move the image
+var img = document.getElementById('img');
+img.onclick = funcrion () {
+    img.style.marginLeft = '100px'; 
+}
