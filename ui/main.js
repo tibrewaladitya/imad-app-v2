c89bd5ad81466d9066cbd1d3e6cd0ec126c3bea8
Aditya Tibrewal <adityatibrewal11@gmail.com>
2017-02-13 13:24:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,5 +12,5 @@ function moveRight() {
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 100);
 };
