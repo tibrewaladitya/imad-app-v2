ab1b26b8d2a2414cd9f6ca853f3da681de3ae4a0
Aditya Tibrewal <adityatibrewal11@gmail.com>
2017-02-13 13:25:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('mudi');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
